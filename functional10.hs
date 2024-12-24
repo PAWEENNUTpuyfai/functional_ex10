b6005690bc9163ee669d77d873dfa4213bc3030d
PAWEENNUTpuyfai <110964154+PAWEENNUTpuyfai@users.noreply.github.com>
2024-12-24 10:27:14 +0700

    challenge

--- functional10.hs
+++ functional10.hs
@@ -14,6 +14,13 @@ elem_fold x l = foldl (\acc e-> if x == e then True else acc) False l
 partition_fold :: Foldable t => (a -> Bool) -> t a -> ([a], [a])
 partition_fold p l = foldr (\x (accf,accs) -> if p(x) then (x:accf,accs) else (accf,x:accs) ) ([],[]) l
 
+--challenge (extra credits):
+--  define foldl in terms of foldr
+reverse_right :: Foldable t => t a -> [a]
+reverse_right l = foldr (\x acc -> acc ++ [x]) [] l
+
+foldl' :: Foldable t => (a -> b -> a) -> a -> t b -> a
+foldl' f acc l =foldr (\x acc-> flip(f) x acc) acc (reverse_right (l))
 
 --define type Month whose values are months in a year
 data Month = January | February | March | April | May | June | July |
@@ -53,3 +60,16 @@ nextDay :: Integer -> Month -> (Integer, Month)
 nextDay d m 
     | d == daysInMonth m  =  (1,nextMonth m)
     | otherwise            =   (d+1,m)
+
+
+
+
+--test challenge
+filterFoldl :: Foldable t => (a -> Bool) -> t a -> [a]
+filterFoldl pred = foldl' (\acc x -> if pred x then acc ++ [x] else acc) []
+
+mapFoldl :: Foldable t1 => (t2 -> a) -> t1 t2 -> [a]
+mapFoldl f xs = reverse (foldl' (\acc x -> f x : acc) [] xs)
+
+reverse_left :: Foldable t => t a -> [a]
+reverse_left l = foldl' (flip (:)) [] l 
